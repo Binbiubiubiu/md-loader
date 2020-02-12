d5b1a81cacb91fc3d5fb3b5ebd0e585ed944eae0
lvbin <2632003786@qq.com>
2020-02-12 17:17:55 +0800

    :bug: fixed entry url

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "example/index.js",
+  entry: "./example/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
